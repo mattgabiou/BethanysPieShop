78726a6442499f0ebaabf86c307206f49a9c7965
Matt Gabiou <mgabiou@engagehealth.com>
2025-11-24 17:43:47 -0600

    Pie Class

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,25 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Makes sure our application knows about ASP.NET Core MVC by bringing in framework services that enable MVC
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 
+// Begin bringing in Middleware components and this starts the Middleware Component Pipeline
 // My custom code below (2025-11-16)
-app.UseStaticFiles();
-//app.mapstasticassetts // New version of use static files. Not used on demo
+app.UseStaticFiles(); // Middleware Component
+//app.mapstasticassetts // New version of use static files. Not used on demo. Middleware Component
 
 
-// Checks the environment variable that if in develop mode, shows the error ouput for debugging
+// Diagnostic Middleware Component that checks the environment variable that if in develop mode, shows the error ouput for debugging
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 
 
-// Gives the ability to navigate to our views. It lets MVC handle our incoming requets
+// Endpoint Middleware Component that gives the ability to navigate to our views. It lets MVC handle our incoming requets
 app.MapDefaultControllerRoute();
+// End Middleware Components
 
 
-
-// End custom code
-//app.MapGet("/", () => "Hello World!");
-
+// Starts the application
 app.Run();
