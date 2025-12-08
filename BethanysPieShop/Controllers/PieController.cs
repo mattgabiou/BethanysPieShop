73c8600821723b420643030bf4c8dd3acecc2065
Matt Gabiou <mgabiou@engagehealth.com>
2025-12-08 17:17:26 -0600

    Created PieController

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,23 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pierepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            // Constructor Injection
+            _pierepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List() // Action Method
+        {
+            return View(_pierepository.AllPies);
+        }
+    }
+}
